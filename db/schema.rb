59a3f84099d749cef96a107f94c58185bdd952a5
Saïf <saifbacari@gmail.com>
2020-01-16 21:14:31 +0100

    controller ajouté

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_14_204354) do
+ActiveRecord::Schema.define(version: 2020_01_16_193658) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "services", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "category"
+    t.string "name"
+    t.string "description"
+    t.integer "price_hourly"
+    t.integer "price_daily"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -23,14 +33,8 @@ ActiveRecord::Schema.define(version: 2020_01_14_204354) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "sexe"
-    t.integer "age"
-    t.string "photo"
-    t.text "description"
-    t.integer "height"
-    t.string "alcool"
-    t.string "tabac"
-    t.string "address"
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
