54e0861a27632fab14cea7824815b701d914f043
Ziad Ghalleb <ziad.ghalleb@protonmail.com>
2020-01-16 20:42:47 +0100

    Created Service Model and Service DB Migration with corresponding attributes

--- db/schema.rb
+++ db/schema.rb
@@ -23,8 +23,14 @@ ActiveRecord::Schema.define(version: 2020_01_14_204354) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "first_name"
-    t.string "last_name"
+    t.string "sexe"
+    t.integer "age"
+    t.string "photo"
+    t.text "description"
+    t.integer "height"
+    t.string "alcool"
+    t.string "tabac"
+    t.string "address"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
