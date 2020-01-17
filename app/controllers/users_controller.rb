4bc4a6a8fa6f0284d7f0e58c734c2a1fb6799da2
Ziad Ghalleb <ziad.ghalleb@protonmail.com>
2020-01-17 03:13:03 +0100

    populated user/services/bookings controllers with corresponding CRUD methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,33 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :update, :destroy]
 
   def index
+    @users = User.all
   end
 
   def show
   end
 
-  def new
-  end
-
-  def create
-  end
-
-  def edit
-  end
-
   def update
+    @user.update(user_params)
+
+    redirect_to user_path(@user)
   end
 
   def destroy
+    @user.destroy
+
+    redirect_to users_path
+  end
+
+  private
+
+  def set_user
+    @user = Users.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:description, :height, :alcool, :tabac, :address)
   end
 
 end
