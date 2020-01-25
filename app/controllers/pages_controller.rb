9a14469e25a57e930b1afb7a52746667cacfd228
Ziad Ghalleb <ziad.ghalleb@protonmail.com>
2020-01-25 12:50:10 +0100

    commit before merge master

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,15 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home, :index, :show]
+  # before_action :set_user, only: [:dashboard]
 
   def home
     @services = Service.all
     @users = User.all
+  end
+
+  def dashboard
+    @user = current_user
+    # @booking.user = current_user
+    # @booking.user_id = params[:service_id]
   end
 end
